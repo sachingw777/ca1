8294401b83ff23bff37ec798e04e052b57dab0ab
Sachin Gwasikoti <sachingw777@gmail.com>
2020-08-30 18:45:45 +0530

    check if db is causing problems for deployment.

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express 	= require("express"),
 //local host
 // mongoose.connect("mongodb://localhost/ca_db",{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
 
-let url = process.env.DATABASEURL;
-mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
+// let url = process.env.DATABASEURL;
+// mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -19,8 +19,8 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-// app.listen(3057, function(){
-//   console.log("APP.JS STARTED");
-// });
+app.listen(3060, function(){
+  console.log("APP.JS STARTED");
+});
 
-app.listen(process.env.PORT, process.env.IP, () => console.log("Server Has Started!"));
+// app.listen(process.env.PORT, process.env.IP, () => console.log("Server Has Started!"));
