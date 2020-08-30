20f9f06f1684e8de3dd97c51c3a1a33de38a8e7c
Sachin Gwasikoti <sachingw777@gmail.com>
2020-08-30 18:14:24 +0530

    add heroku fix.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var express 	= require("express"),
     mongoose    = require("mongoose"),
     Customer 	= require("./models/customers");
 
-mongoose.connect("mongodb://localhost/ca_db",{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
+
+let url = process.env.DATABASEURL;
+mongoose.connect(url,{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -15,6 +17,6 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.listen(3057, function(){
+app.listen(3059, function(){
   console.log("APP.JS STARTED");
 });
