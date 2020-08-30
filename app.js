a2d2fd315e52ce85b8edfaf17d881a360321f82e
Sachin Gwasikoti <sachingw777@gmail.com>
2020-08-30 17:00:25 +0530

    Add Schema.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express 	= require("express"),
     app 		= express(),
-	bodyParser  = require("body-parser");
+	bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Customer 	= require("./models/customers");
 
+
+mongoose.connect("mongodb://localhost/ca_db",{ useUnifiedTopology: true, useNewUrlParser: true }); //create yelpcamp db inside mongodb
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+// seedDB();
 
 
 app.get("/", function(req, res) {
-    res.render("index1");
+    res.render("index");
 });
 
-app.listen(3033, function(){
+app.listen(3056, function(){
   console.log("APP.JS STARTED");
 });
